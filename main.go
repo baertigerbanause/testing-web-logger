e047f9d53b6ff09a009fcda6e86fbd3c9e675778
baertigerbanause <84443521+baertigerbanause@users.noreply.github.com>
2021-05-19 17:46:33 +0200

    Update main.go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func handlerHealth(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[ %s ] [ %s ] : %s\n", r.RemoteAddr, r.Method, r.URL)
+	fmt.Fprint(w, "OK\n", r.URL.Path[1:])
+}
+func handlerBase(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[ %s ] [ %s ] : %s\n", r.RemoteAddr, r.Method, r.URL)
+	http.Redirect(w, r, "/health", http.StatusFound)
+}
+
+func main() {
+	portPtr := flag.Int("port", 6443, "port to listen on")
+	flag.Parse()
+	port := ":" + strconv.Itoa(*portPtr)
+
+	http.HandleFunc("/health", handlerHealth)
+	http.HandleFunc("/", handlerBase)
+	log.Println("registered handlers")
+	log.Println("Listening on", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
